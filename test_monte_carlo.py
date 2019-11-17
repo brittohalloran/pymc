32ed6b51509302269659be479a19cd43255805fe
Britt O'Halloran <britt.j.ohalloran@gmail.com>
2019-11-17 06:58:08 -0600

    Convert test to pytest

--- test_monte_carlo.py
+++ test_monte_carlo.py
@@ -1,34 +1,31 @@
-import unittest
+import pytest
 import monte_carlo as mc
 
 
-class TestMonteCarlo(unittest.TestCase):
-    def test_Norm(self):
+def test_Norm():
 
-        # Works normally
-        a = mc.Norm(interval=(-1.645, 1.645))
-        self.assertEqual(a.mean, 0)
-        self.assertTrue(abs(a.sd - 1) < 0.005, a.sd)
+    # Works normally
+    a = mc.Norm(interval=(-1.645, 1.645))
+    assert a.mean == 0
+    assert abs(a.sd - 1) < 0.005
 
-        # Bad combination of inputs
-        with self.assertRaises(Exception):
-            a = mc.Norm(mean=0, interval=(0, 1))
+    # Bad combination of inputs
+    with pytest.raises(Exception):
+        a = mc.Norm(mean=0, interval=(0, 1))
 
-        # Bad combination of inputs
-        with self.assertRaises(Exception):
-            a = mc.Norm(mean=0, sd=1, interval=(0, 1))
+    # Bad combination of inputs
+    with pytest.raises(Exception):
+        a = mc.Norm(mean=0, sd=1, interval=(0, 1))
 
-        # Three number interval
-        with self.assertRaises(Exception):
-            a = mc.Norm(interval=(0, 1, 2))
+    # Three number interval
+    with pytest.raises(Exception):
+        a = mc.Norm(interval=(0, 1, 2))
 
-        # One number interval
-        with self.assertRaises(Exception):
-            a = mc.Norm(interval=(0))
-
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 3)), 6, "Should be 6")
+    # One number interval
+    with pytest.raises(Exception):
+        a = mc.Norm(interval=(0))
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_sum_tuple():
+    assert sum((1, 2, 3)) == 6
+
